7af7e81f04d68912aacf1dd98727fc6e17419618
Adit Damodar <aditidamodar23@gmail.com>
2024-11-09 14:46:01 +0530

    Fifth Commit

--- AboutMe/settings.py
+++ AboutMe/settings.py
@@ -115,9 +115,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+# Add this if you want to include static files for your app
+STATICFILES_DIRS = [BASE_DIR / "static"]                       
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
